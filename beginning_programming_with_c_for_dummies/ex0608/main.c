8a6167da50cd8731bf3526d40fb5fa4896dba3af
Devin Smoot <smootd@student.swosu.edu>
2017-07-04 02:03:04 -0500

    Added more examples Ch5 and Ch6

---
+++ beginning_programming_with_c_for_dummies/ex0608/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int shadrach, meshach, abednego;
+
+    shadrach = 701;
+    meshach = 709;
+    abednego = 719;
+
+    printf("Shadrach is %d\nMeschach is %d\nAbednego is %d\n",shadrach,meshach,abednego);
+    return 0;
+}
